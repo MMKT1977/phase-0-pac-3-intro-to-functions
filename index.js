834a9b2df00bd0acedf62e37b5e7f6bde0817d87
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-21 01:34:17 +0300

    FInished Practice

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Follow along with the examples here
+//console.log("Hello, World!");
+
+function add(x, y){
+    return x+y;
+}
+console.log(add(1,2));
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Howdy", "partner"));
